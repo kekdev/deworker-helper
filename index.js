f4f647354dd8346c60e9937c156ef56090626d88
kekdev <6170034+kekdev@users.noreply.github.com>
2021-08-21 01:58:26 +0500

    mark watched videos

--- index.js
+++ index.js
@@ -1,46 +1,93 @@
-const episodePathRegexp = new RegExp('/edu/series/.+'))
-const storageKey = '_viewed'
+// ==UserScript==
+// @name deworker-helper
+// @description remember viewed videos
+// @version 0.0.1
+// @match https://deworker.pro/edu*
+// ==/UserScript==
 
-if (episodePathRegexp.test(window.location.pathname)) {
-  episode();
+const storageKey = '_watchProgress'
+const watchProgress = JSON.parse(localStorage.getItem(storageKey)) || {}
+const urlHandlers = {
+  '^/edu$': episodeList,
+  '^/edu/series/.+': episode,
 }
+const baseUrl = 'https://deworker.pro'
 
-if (window.location.pathname === "/edu") {
-    const viewed = localStorage.getItem(storageKey)
-    if (! viewed) {
-      return;
-    }
-    for (const link of document.querySelector('.edu-items-item a.thumb')) {
-      if (viewed[link.href]) {
-        link.classList.add('viewed')
-      }
-    }
+const watchedClass = 'watched'
+
+for (const regexp in urlHandlers) {
+  if ((new RegExp(regexp)).test(window.location.pathname)) {
+    urlHandlers[regexp]();
+  }
 }
 
 async function episode() {
-    const player = await loadPlayer()
-    player.on('ended', () => markAsViewed(window.location.pathname))
-    
+  const player = await loadPlayer()
+  player.on('ended', () => {
+    markAsViewed(window.location.pathname)
+  })
 }
-    
+
+function episodeList() {
+  injectStyles()
+  for (const link of document.querySelectorAll('.edu-items-item a.thumb')) {
+    if (watchProgress[link.href.replace(baseUrl, '')] == 100) {
+      link.classList.add(watchedClass)
+    }
+  }
+}
+
 function loadPlayer() {
   const script = document.createElement('script')
   script.src = 'https://player.vimeo.com/api/player.js'
-  
-  return new Promize((resolve, reject) => {
-    script.addEventListener('onload', () => {
+  document.body.appendChild(script)
+
+  return new Promise((resolve, reject) => {
+    script.addEventListener('load', () => {
       resolve(new Vimeo.Player(document.querySelector('iframe')))
     })
-    document.body.appendChild('script')
+  })
 }
 
 function markAsViewed(url) {
-  localStorage.setItem(
-    storageKey,
-    {
-      ...(localStorage.getItem(storageKey) || {}),
-      [url]: true
-    }
-  )
+  watchProgress[url] = 100
+  updateStorage()
 }
-  
+
+function injectStyles() {
+  const styles = `
+.edu-items .thumb.watched::before {
+  background-color: rgba(33,33,33,.6);
+  display: block;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  color: white;
+}
+
+.edu-items-item .thumb.watched::after {
+    display: block;
+    position: absolute;
+    z-index: 2;
+    border-top: 1px solid #fff;
+    border-bottom: 1px solid #fff;
+    content: 'Просмотрено';
+    color: #fff;
+    background-color: rgba(33,33,33,.9);
+    text-align: center;
+    transform: translateY(-50%);
+    font-size: 1.1em;
+    top: 50%;
+    left: 0;
+    width: 100%;
+}
+`
+  const tag = document.createElement('style')
+  tag.innerText = styles
+  document.head.appendChild(tag)
+}
+
+function updateStorage() {
+  localStorage.setItem(storageKey, JSON.stringify(watchProgress))
+}
